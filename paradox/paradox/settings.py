3353280e7747e2d05af89d7660b80e5adb492a2f
https://18020777@github.com <selim.dleyssac@protonmail.com>
2022-10-16 16:39:41 +0200

    Configuration with MariaDB, app started, superuser created.

--- paradox/paradox/settings.py
+++ paradox/paradox/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'paradox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'paradox',
+        'USER': 'paradoxuser',
+        'PASSWORD': 'tiapfpEG2022',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
