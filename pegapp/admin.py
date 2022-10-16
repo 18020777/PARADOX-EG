6144f9cb3e4ad16ce70e830b2bfb295554af4e1e
https://18020777@github.com <selim.dleyssac@protonmail.com>
2022-10-16 17:16:57 +0200

    Scenario class created (missing image gallery), added to admin.

--- pegapp/admin.py
+++ pegapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+import pegapp.models as pm
+
+
+class ScenarioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration', 'difficulty')
+
+
+admin.site.register(pm.Scenario, ScenarioAdmin)
 # Register your models here.
--- pegapp/models.py
+++ pegapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Scenario(models.Model):
+    class Difficulty(models.IntegerChoices):
+        very_easy = 1
+        easy = 2
+        normal = 3
+        hard = 4
+        very_hard = 5
+
+    name = models.fields.CharField(max_length=50)
+    description = models.fields.CharField(max_length=1000)
+    duration = models.fields.DurationField(default="01:00:00")
+    # images = models.field
+    difficulty = models.fields.IntegerField(choices=Difficulty.choices)
