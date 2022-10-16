3cf80b8876c98c102a3455fe26e01b9470c6519d
https://18020777@github.com <selim.dleyssac@protonmail.com>
2022-10-16 19:51:55 +0200

    Images gallery added for every scenario, nb players added.

--- paradox/pegapp/admin.py
+++ paradox/pegapp/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 import pegapp.models as pm
 
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'id')
+
+
 class ScenarioAdmin(admin.ModelAdmin):
     list_display = ('name', 'duration', 'difficulty')
 
 
+admin.site.register(pm.Image, ImageAdmin)
 admin.site.register(pm.Scenario, ScenarioAdmin)
-# Register your models here.
---
+++ paradox/pegapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='pegapp/uploads/images/gallery/')
+
+    def __str__(self):
+        return str(self.image)
+
+
+class Scenario(models.Model):
+    class Difficulty(models.IntegerChoices):
+        very_easy = 1
+        easy = 2
+        normal = 3
+        hard = 4
+        very_hard = 5
+
+    name = models.fields.CharField(max_length=50)
+    description = models.fields.TextField(default="No description available.", max_length=1000)
+    duration = models.fields.DurationField(default="01:00:00")
+    difficulty = models.fields.IntegerField(choices=Difficulty.choices)
+    min_players = models.fields.IntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    max_players = models.fields.IntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    images = models.ManyToManyField(Image)
