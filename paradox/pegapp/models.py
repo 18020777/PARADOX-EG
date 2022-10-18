6d11968772311b723fb74b5841a85e3409cb9565
https://18020777@github.com <selim.dleyssac@protonmail.com>
2022-10-18 23:00:50 +0200

    PEG-2 A scenario can now have 0 image.

--- paradox/pegapp/models.py
+++ paradox/pegapp/models.py
@@ -23,4 +23,4 @@ class Scenario(models.Model):
     difficulty = models.fields.IntegerField(choices=Difficulty.choices)
     min_players = models.fields.IntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(10)])
     max_players = models.fields.IntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(10)])
-    images = models.ManyToManyField(Image)
+    images = models.ManyToManyField(Image, blank=True)
