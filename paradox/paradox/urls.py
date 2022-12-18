9c73b99c757ba2fc4f8e245383972649f3b5678b
https://18020777@github.com <selim.dleyssac@protonmail.com>
2022-12-19 00:05:32 +0100

    PEG-17 Possibility to go on a scenario details' page (empty at the moment). Dropdown added with pure CSS without Bootstrap. Project tree corrected for static files.

--- paradox/paradox/urls.py
+++ paradox/paradox/urls.py
@@ -13,17 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import path
 from pegapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.base),
+    path('scenario/<int:scn_id>/', views.scenario_detail, name='scenario-detail'),
+    path('', views.home, name='home'),
 ]
 
 if settings.DEBUG:
--- paradox/pegapp/views.py
+++ paradox/pegapp/views.py
@@ -6,6 +6,12 @@ from pegapp import models as m
 # from . import forms
 
 
-def base(request):
+def home(request):
     scenarios = m.Scenario.objects.all()
     return render(request, 'theme/home.html', {'scenarios': scenarios})
+
+
+def scenario_detail(request, scn_id):
+    scenarios = m.Scenario.objects.all()
+    this_scenario = m.Scenario.objects.get(id=scn_id)
+    return render(request, 'theme/scenario.html', {'scenarios': scenarios, 'this_scenario': this_scenario})
