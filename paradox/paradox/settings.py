d570b171ddc3ce15641e8ade9ed181ae8194c10a
https://18020777@github.com <selim.dleyssac@protonmail.com>
2023-01-05 22:45:15 +0100

    PEG-17 Possibility to go on a scenario details' page (empty at the moment). Dropdown added with pure CSS without Bootstrap. Project structure changed with static and templates in the pegapp. Settings fixed. Scss compressor added, bootstrap added with scss and imported in style.scss.

--- paradox/paradox/settings.py
+++ paradox/paradox/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+APP_DIR = os.path.join(BASE_DIR, 'pegapp')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -37,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pegapp',
+    'compressor',
 ]
 
-# MEDIA_URL = '/'
-# MEDIA_ROOT = BASE_DIR.joinpath('/')
+MEDIA_ROOT = os.path.join(APP_DIR, 'media')
+MEDIA_URL = '/media/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'paradox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(APP_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pegapp/static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(APP_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(APP_DIR, 'static')
+STATICFILES_FINDERS = ['compressor.finders.CompressorFinder']
+
+COMPRESS_PRECOMPILERS = (('text/x-scss', 'django_libsass.SassCompiler'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
